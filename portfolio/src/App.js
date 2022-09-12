520b50c5e35e236ec8869784c6844f1131c856f6
unknown <jbhbprograming@gmail.com>
2022-09-12 06:25:20 -0400

    about me

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="back">
+          <span className="fil-fold">
+            <div className="fold">
+              <Header/>
+              <div className="intro">
+                <h1>
+                  Hellow World  I`m 
+                </h1>
+                <h1 className="name">
+                  {'<h1> Jose Bastidas </h1>'}
+                </h1>
+                <h1>
+                  Front End Developer
+                </h1>
+              </div>
+            </div>
+            <div className="logo"></div>
+          </span>
+          <button className="conta">Contact Me</button>
+          <p className="whater">{'</>'}</p>
+        </div>
+        <div className="Container">
+          <h1>
+            {'<About Me/>'}
+          </h1>
+        </div>
     </div>
   );
 }
